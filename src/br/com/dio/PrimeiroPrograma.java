ba43280044967667f932b2d87488535a34a5f900
Tiago Costa <42659690+tiagocostaazevedo@users.noreply.github.com>
2022-03-08 16:16:12 -0300

    Primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+
+
+
+        /*  int a = 5 ;
+            int b = 3 ;
+
+        System.out.println("Hello World!" + (a+b));
+         */
+    }
+}
